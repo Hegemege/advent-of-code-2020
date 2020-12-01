c5659d5e9fb0d51e5cf9ea0089d9671fe184c95f
Henri Niva <henri.a.niva@gmail.com>
2020-12-01 21:54:24 +0200

    Faster d1

--- 01/solution.py
+++ 01/solution.py
@@ -1,23 +1,29 @@
 def part1(input_data):
-    for j in range(len(input_data)):
-        for i in range(j, len(input_data)):
-            if input_data[j] + input_data[i] == 2020:
-                print(input_data[j] * input_data[i])
-                return
+    # Assumes that input contains no duplicates and not the exact half
+    # of the sum, i.e. 1010
+    input_lookup = set(input_data)
+
+    for item in input_data:
+        if 2020 - item in input_lookup:
+            return (2020 - item) * item
 
 
 def part2(input_data):
-    for k in range(len(input_data)):
-        for j in range(k, len(input_data)):
-            for i in range(j, len(input_data)):
-                if input_data[k] + input_data[j] + input_data[i] == 2020:
-                    print(input_data[k] * input_data[j] * input_data[i])
-                    return
+    input_lookup = set(input_data)
+
+    for j in range(len(input_data)):
+        for i in range(j, len(input_data)):
+            if 2020 - input_data[i] - input_data[j] in input_lookup:
+                return (
+                    (2020 - input_data[i] - input_data[j])
+                    * input_data[i]
+                    * input_data[j]
+                )
 
 
 if __name__ == "__main__":
     with open("input", "r") as input_file:
         input_data = list(map(lambda x: x.strip(), input_file.readlines()))
         input_data = list(map(int, input_data))
-        part1(input_data)
-        part2(input_data)
+        print(part1(input_data))
+        print(part2(input_data))

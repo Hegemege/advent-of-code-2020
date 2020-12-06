bcc8f7c6cda2d0e48ecac3e194af5cffd77a99bb
Henri Niva <henri.a.niva@gmail.com>
2020-12-06 21:45:51 +0200

    Day 06 part 2

--- 06/solution.py
+++ 06/solution.py
@@ -8,7 +8,15 @@ def part1(input_data):
 
 
 def part2(input_data):
-    pass
+    data = functools.reduce(aggregate_group_part2, input_data, [{"size": 0}])
+    return sum(
+        map(
+            lambda x: sum(
+                [1 if v == x["size"] and k != "size" else 0 for k, v in x.items()]
+            ),
+            data,
+        )
+    )
 
 
 def aggregate_group(a, b):
@@ -16,6 +24,18 @@ def aggregate_group(a, b):
         a.append(set())
     else:
         a[-1].update(b)
+    return a
+
+
+def aggregate_group_part2(a, b):
+    if len(b) == 0:
+        a.append({"size": 0})
+    else:
+        for symbol in b:
+            if symbol not in a[-1]:
+                a[-1][symbol] = 0
+            a[-1][symbol] += 1
+        a[-1]["size"] += 1
     return a
 
 

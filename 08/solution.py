e507f40267992edf8477dd3809085980ce2945d7
Henri Niva <henri.a.niva@gmail.com>
2020-12-08 14:49:59 +0200

    Day 08 part 2

--- 08/solution.py
+++ 08/solution.py
@@ -25,7 +25,41 @@ def part1(input_data):
 
 
 def part2(input_data):
-    pass
+    bootcode = list(map(lambda x: x.split(" "), input_data))
+    bootcode = list(map(lambda x: (x[0], int(x[1])), bootcode))
+
+    for i in range(len(bootcode)):
+        modified_bootcode = list(bootcode)
+        if modified_bootcode[i][0] == "acc":
+            continue
+        elif modified_bootcode[i][0] == "nop":
+            modified_bootcode[i] = ("jmp", modified_bootcode[i][1])
+        elif modified_bootcode[i][0] == "jmp":
+            modified_bootcode[i] = ("nop", modified_bootcode[i][1])
+
+        cursor = 0
+        accumulator = 0
+        visited = set()
+
+        while True:
+            if cursor in visited:
+                break
+            visited.add(cursor)
+
+            if cursor < 0:
+                break
+
+            if cursor == len(modified_bootcode):
+                return accumulator
+
+            code = modified_bootcode[cursor]
+            if code[0] == "nop":
+                cursor += 1
+            elif code[0] == "acc":
+                accumulator += code[1]
+                cursor += 1
+            elif code[0] == "jmp":
+                cursor += code[1]
 
 
 if __name__ == "__main__":

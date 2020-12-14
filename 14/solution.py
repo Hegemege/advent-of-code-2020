3928584674ea4ec9f1863b478102ec114de70adc
Henri Niva <henri.a.niva@gmail.com>
2020-12-14 17:39:42 +0200

    Day 14 part 2

--- 14/solution.py
+++ 14/solution.py
@@ -30,7 +30,38 @@ def apply_mask(value, mask):
 
 
 def part2(input_data):
-    pass
+    mask = "0" * 36
+    memory = {}
+
+    for command in input_data:
+        if "mask" in command:
+            mask = command.split(" = ")[1]
+            continue
+        address, value = command.split(" = ")
+        address = int(address[4:-1])
+        address = apply_mask_part2(address, mask)
+        value = int(value)
+        for fixed_address in resolve_floating_address(address):
+            memory[fixed_address] = value
+
+    return sum(memory.values())
+
+
+def apply_mask_part2(value, mask):
+    value_bits = list("{0:036b}".format(value))
+    for i in range(36):
+        if mask[i] != "0":
+            value_bits[i] = mask[i]
+    return "".join(value_bits)
+
+
+def resolve_floating_address(address):
+    if "X" not in address:
+        yield int(address, 2)
+        return
+
+    yield from resolve_floating_address(address.replace("X", "0", 1))
+    yield from resolve_floating_address(address.replace("X", "1", 1))
 
 
 if __name__ == "__main__":
